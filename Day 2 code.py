7d24e31cb0a70fcacf8e929cf1dbe43a53ac967c
rutujanalage <68290303+rutujanalage@users.noreply.github.com>
2020-07-24 14:38:32 +0530

    Create Day 2 code.py

---
+++ Day 2 code.py
@@ -0,0 +1,12 @@
+Operators
+
+24 july Friday 
+@author- Rutuja Nalage 
+
+meal_cost=float(input())
+tip_percent=int(input())
+tax_percent=int(input())
+tip=meal_cost*tip_percent/100
+tax=meal_cost*tax_percent/100
+total=meal_cost+tip+tax
+Print ("the total meal cost is"+str(round(total))+"dollars.")
