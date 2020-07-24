fb8eeb294f01385e5bd74f2d12ceea4539517909
rutujanalage <68290303+rutujanalage@users.noreply.github.com>
2020-07-24 14:24:07 +0530

    Create Mod Divmod.py

---
+++ Mod Divmod.py
@@ -0,0 +1,10 @@
+# HackerRank.-py
+30 days of code python 
+  Friday 24 July
+ @author-Rutuja Nalage 
+
+   a=int(raw_input())
+   b=int(raw_input())
+   print a/b
+   print a%b
+   print divmod(a,b)
