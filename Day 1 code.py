8450731d1308eda646357d36bec3b9f26b9e59aa
rutujanalage <68290303+rutujanalage@users.noreply.github.com>
2020-07-24 14:31:54 +0530

    Create Day 1 code.py

---
+++ Day 1 code.py
@@ -0,0 +1,13 @@
+Data type
+24 july friday
+@author- Rutuja Nalage 
+
+i=4
+d=4.0
+s="Hackerrank"
+a=12
+b=4.0
+c="is the best place to learn and practice coding"
+print(i+a)
+Print(d+b)
+Print(s+c)
